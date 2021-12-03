6c0da1f3fa5a254c5feb56af4641f731a2a40c9c
kamikarp <duong.r@northeastern.edu>
2021-12-02 21:28:12 -0500

    d

--- src/model/Posn.java
+++ src/model/Posn.java
@@ -1,11 +1,6 @@
 package model;
 
-public class Posn {
-  private final int x;
-  private final int y;
-
-  public Posn(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+public interface Posn {
+  int getX();
+  int getY();
 }
---
+++ src/model/PosnImpl.java
@@ -0,0 +1,21 @@
+package model;
+
+public class PosnImpl implements Posn {
+  private final int x;
+  private final int y;
+
+  public PosnImpl(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public int getX() {
+    return x;
+  }
+
+  @Override
+  public int getY() {
+    return y;
+  }
+}
